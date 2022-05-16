8a06c9044eefe67db5ad3387139698ea29355351
ya.novikov <arhdomain@gmail.com>
2022-05-16 14:33:12 +0300

    5

--- create_bot.py
+++ create_bot.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher import Dispatcher
 import os
 
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = '5184650042:AAG69ZA5Tu7n_Lx50LvucubhfCl_d-EF6xg'
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
